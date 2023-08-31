85285ddce10d154a605cea0006c071bc63a73939
Lucas Vieira <ls6182315@gmail.com>
2023-08-31 15:08:43 -0300

    feat:add api route to send messages into database

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,10 +1,18 @@
-import { sendDataToStorage } from '@/firebase/firebase-auth'
+import prisma from '@/lib/prismadb'
+
 import { NextRequest, NextResponse } from 'next/server'
 
 export async function POST(request: NextRequest) {
   try {
     const body = await request.json()
     const { message, chatId } = body
+
+    await prisma.message.create({
+      data: {
+        body: message,
+        chatId,
+      },
+    })
 
     const API_URL = process.env.NEXT_PUBLIC_OPEN_AI_API_URL
     const requestOptions = {
@@ -18,14 +26,20 @@ export async function POST(request: NextRequest) {
         messages: [{ role: 'user', content: message }],
       }),
     }
+    // data.choices[0].message.content, chatId, true
+    const response = await fetch(API_URL as string, requestOptions)
+    const data = await response.json()
+    const botMessage = data.choices[0].message.content
 
-    await fetch(API_URL as string, requestOptions)
-      .then((res) => res.json())
-      .then((data) => {
-        sendDataToStorage(data.choices[0].message.content, chatId, true)
-      })
+    await prisma.message.create({
+      data: {
+        body: botMessage,
+        chatId,
+        isBot: true,
+      },
+    })
 
-    return NextResponse.json({ message: 'success' })
+    return NextResponse.json({ body: botMessage, isBot: true })
   } catch (error) {
     console.error(error)
     return new NextResponse('Error', { status: 500 })
