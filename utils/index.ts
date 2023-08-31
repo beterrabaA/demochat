f4ee0399c34aee2da89c1f3265f3e73750dfcb10
Lucas Vieira <ls6182315@gmail.com>
2023-08-31 14:55:46 -0300

    feat:add mapperAndCsvConversor utils function

--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { convertArrayToCSV } from 'convert-array-to-csv'
+
+import { IChatHistory } from '@/types'
 
 export async function getList(id: string) {
   const { data } = await axios.post('/api/messages/list', {
@@ -6,4 +9,15 @@ export async function getList(id: string) {
   })
 
   return data
+}
+
+export function mapperAndCsvConversor(data: IChatHistory[]) {
+  const mappedData = data.map((item: IChatHistory) => ({
+    message: item.body,
+    createdAt: item.createdAt,
+  }))
+
+  const csvFromArrayOfObjects = convertArrayToCSV(mappedData)
+
+  return csvFromArrayOfObjects
 }
